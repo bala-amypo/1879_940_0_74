a2fd3b978dde5528fb9f1c5304a20dd5d1b252c7
bala-amypo <bala@amypo.in>
2025-12-18 08:56:30 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,64 +1,63 @@
 package com.example.demo.entity;
-import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
-import jakarta.persistence.Column;
 
 @Entity
- @Table(name="StudentTable")
 public class Student {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @NotBlank(message=" Name field cannot be empty")
-    @Size(min=3,max=20,message="The user name must be of min 3 and max 20 character")
+    private long id;
+
+    @NotBlank(message = "Name field cannot be empty")
+    @Size(min = 3, max = 20, message = "The user name must be min of 3 and max of 20 characters")
     private String name;
-    @Email(message="Invalid email id")
-    @Column(unique=true)
+
+    @Email(message = "Invalid email id")
+    @NotBlank(message = "Email cannot be empty")
+    @Column(unique = true)
     private String email;
-    private Int age;
-    //@NotNull
-    //@Size(min,max)(to reccomend the use to the give no of characters)
-    //@Min
-    //@Max
-    //@Pattern(reg)(used for phone numbers)
-    //@Positive(used in bank record detailling)
-    public Long getId() {
+
+    // Constructors
+    public Student() {
+    }
+
+    public Student(long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters & Setters
+    public long getId() {
         return id;
     }
-    public void setId(Long id) {
+
+    public void setId(long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
-     public float getage() {
-        return age;
-    }
-    public void setCgpa(float age) {
-        this.age = age;
-    }
-    public Student(Long id, String name, String email,Int age) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.age = age;
-    }
-    public Student() {
-    } 
 }
