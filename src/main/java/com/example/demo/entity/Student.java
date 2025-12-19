cf34c7a407b498be7c242c975705b1930e21810f
bala-amypo <bala@amypo.in>
2025-12-19 06:25:39 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -31,36 +31,4 @@ public class Student {
     @Column(unique = true)
     private String email;
 
-    public Student() {
-    }
-
-    public Student(long id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
